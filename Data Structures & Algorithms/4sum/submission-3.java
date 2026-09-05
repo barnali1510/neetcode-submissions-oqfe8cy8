4f6438d4b937636b45ade5b83718db4a54844358
barnali1510 <73812587+barnali1510@users.noreply.github.com>
2026-09-05 23:11:47 +0530

    Add: 4sum - submission-3

---
+++ Data Structures & Algorithms/4sum/submission-3.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        int n = nums.length;
+        Set<List<Integer>> result = new HashSet<>();
+
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                for(int k=j+1; k<n; k++) {
+                    for(int l=k+1; l<n; l++) {
+                        if((long) nums[i] + nums[j] + nums[k] + nums[l] == target) {
+                            List<Integer> temp  = new ArrayList<>();
+
+                            temp.add(nums[i]);
+                            temp.add(nums[j]);
+                            temp.add(nums[k]);
+                            temp.add(nums[l]);
+
+                            Collections.sort(temp);
+                            result.add(temp);
+                        }
+                    }
+                }
+            }
+        }
+
+        List<List<Integer>> ans = new ArrayList<>(result);
+        return ans;
+        
+    }
+}
